28bfb8f1f59c6cbd118ffbd2c0dc50b98d2d79d5
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2022-01-02 07:34:57 +0600

    Initial commit

---
+++ src/main/java/com/hasan/crowdcarnival/CrowdCarnivalApplication.java
@@ -0,0 +1,13 @@
+package com.hasan.crowdcarnival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdCarnivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdCarnivalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasan/crowdcarnival/CrowdCarnivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.crowdcarnival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdCarnivalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
